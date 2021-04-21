4b5ae83c510821e3fa4a3534e9940c8b6964b717
mark <mark@used.ca>
2021-04-21 15:52:55 -0700

    Added option to choose the number of tracking cycles and how many datapoints per cycle

--- track.py
+++ track.py
@@ -7,33 +7,48 @@ import string
 import multiprocessing
 
 class Track:
-
-    def __init__(self,period,storage_type,forever,mouse_file_path=None,keyboard_file_path=None):
+    
+    def __init__(self,period,storage_type,forever,mouse_file_path=None,keyboard_file_path=None,track_duration_per_dataset=1000,track_cycle_count=10):
         self.period = period
         self.storage_type = storage_type
         self.forever = forever
         if storage_type == 'file':
             self.mouse_file_path = mouse_file_path
         self.keyboard_file_path = keyboard_file_path
+        self.track_duration_per_dataset = track_duration_per_dataset #Not in seconds but in numeber of rows
+        self.track_cycle_count = track_cycle_count
 
-    def track_mouse(self):
+    def store_mouse_data(self,suffix):
         """
-        Tracks your mouse movement every 'given' time period and saves in a csv
+        Stores your mouse movement data every given time period and saves in a csv
         """
         m = PyMouse()
-        while(self.forever):
-            file = open(self.mouse_file_path,'a',newline='')
-            header = ['x','y']
-            writer = csv.DictWriter(file,fieldnames=header)
-            with file:
-                if os.path.exists(self.mouse_file_path) == False:
-                    # header = ['x','y']
-                    # writer = csv.DictWriter(file,fieldnames=header)
-                    writer.writeheader()
-                writer.writerow({'x':round(m.position()[0],1), 'y':round(m.position()[1],1)})
-            print(f'Tracking your mouse movement every {self.period} seconds')
-            time.sleep(self.period)
-
+        file = open(self.mouse_file_path + str(suffix) + ".csv",'a',newline='')
+        header = ['x','y']
+        writer = csv.DictWriter(file,fieldnames=header)
+        with file:
+            if os.path.exists(self.mouse_file_path + str(suffix) + ".csv") == False:
+                # header = ['x','y']
+                # writer = csv.DictWriter(file,fieldnames=header)
+                writer.writeheader()
+            writer.writerow({'x':round(m.position()[0]), 'y':round(m.position()[1])})
+        print(f'Tracking your mouse movement every {self.period} seconds')
+        time.sleep(self.period)
+        
+    def track_mouse(self):
+        """
+        Tracks your mouse movement every so often based on the options you've set
+        Defaults to 1 data point for every 2 seconds until 1000 data points and 
+        repeats 10 times, ie total tracking time of approximately 5.5h
+        """
+        if (self.forever == True):
+            while(self.forever):
+                self.store_mouse_data("FOREVER")
+        else:
+            for cycle in range(self.track_cycle_count):
+                for count in range(self.track_duration_per_dataset):
+                    self.store_mouse_data(cycle)
+            
     def track_keyboard(self):
         """
         Tracks your keyboard presses in the given file.
@@ -47,7 +62,7 @@ class Track:
 
 
 if __name__ == '__main__':
-    track = Track(3,'file',True,'/Users/markshaio/Desktop/Proof_of_concept/Track/Track/track2.csv','/Users/markshaio/Desktop/Proof_of_concept/Track/Track/keys2.txt')
+    track = Track(2,'file',True,'/Users/markshaio/Desktop/Proof_of_concept/Track/Track/TRACK_','/Users/markshaio/Desktop/Proof_of_concept/Track/Track/KEYS.txt')
     mouse_process = multiprocessing.Process(name='mouse_process', target=track.track_mouse)
     keyboard_process = multiprocessing.Process(name='keyboard_process', target=track.track_keyboard)
     mouse_process.start()
